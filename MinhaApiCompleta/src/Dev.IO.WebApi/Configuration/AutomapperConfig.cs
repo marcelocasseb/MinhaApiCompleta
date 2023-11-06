2b3dd2641941847fc7598361a917aa421657d620
marcelo <marcelo.casseb@gmail.com>
2023-11-06 06:09:46 -0300

    criando DTOs e migrations

---
+++ MinhaApiCompleta/src/Dev.IO.WebApi/Configuration/AutomapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Models;
+
+namespace DevIO.Api.Configuration
+{
+    public class AutomapperConfig : Profile
+    {
+        public AutomapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<ProdutoViewModel, Produto>().ReverseMap();
+
+        }
+    }
+}
---
+++ MinhaApiCompleta/src/Dev.IO.WebApi/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+using DevIO.Api.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Dev.IO.WebApi.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+       
+    }
+
+    
+}
